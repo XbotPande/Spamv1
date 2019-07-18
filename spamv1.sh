3b2e3e1d51e0995e162a67de2d7d5a9b5e34db34
MR.XBOTS <52588498+XbotPande@users.noreply.github.com>
2019-07-18 16:24:09 +0700

    Spamv1
    Spam aja

---
+++ spamv1.sh
@@ -0,0 +1,65 @@
+#usr/bin/bash
+clear
+# Recode ajh mmnx
+sleep 0.03
+echo "      _.-^^---....,,-- "
+sleep 0.03
+echo "  _--                  --_ "
+sleep 0.03
+echo " <                        >) "
+sleep 0.03
+echo " |                         | "
+sleep 0.03
+echo "  \._                   _./ "
+sleep 0.03
+echo "     '''--. . , ; .--'''    " 
+sleep 0.03
+echo "           | |   |           "
+sleep 0.03
+echo "        .-=||  | |=-.    "
+sleep 0.03
+echo "        '-=#$%&%$#=-'    "
+sleep 0.03
+echo "           | ;  :|      "
+sleep 0.03
+echo "  _____.,-#%&$@%#&#~,._____ "
+sleep 0.03
+echo "=============================="
+sleep 0.3
+echo "==   SPAM SMS/TLP VERSI GUA =="
+sleep 0.3
+echo "=============================="
+echo
+echo
+echo
+echo
+echo "==========================================="
+sleep 1
+echo  "|AUTHOR   : MR.XBOTS                     |"
+sleep 1
+echo  "|WA       : 08814679274                  |"
+sleep 1
+echo  "|IG.      : Pandark00                    |"
+sleep 1
+echo  "|TOOLS    : pengrusak sistem             |"
+sleep 1
+echo "==========================================="
+echo
+echo
+echo "[ 1 ] Spam SMS"
+echo "[ 2 ] spam TLP"
+read -p ">>>>>> " pil; 
+
+if [ $pil = 1 ]
+then
+clear
+cd AVARD
+bash pil:1.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+cd AVARD
+bash pil:2.sh
+fi
